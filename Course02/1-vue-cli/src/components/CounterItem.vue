dccba6fad9c50701688b8a9d24ea6c61f2aa7bbf
HasanDogann <hasandogann38@gmail.com>
2022-02-09 23:18:21 +0300

    9.2.22 23:18

---
+++ Course02/1-vue-cli/src/components/CounterItem.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div class="container-sm" style="display: inline">
+    <h3 class="mb-2">{{ result }}</h3>
+    <button @click="result++" class="green sm">+</button>
+    <button @click="result--" class="red sm">-</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      result: 0,
+    };
+  },
+};
+</script>
---
+++ Course02/1-vue-cli/src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="container">
+    <p>{{ text }}</p>
+    <input type="form" v-model="text" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
